43d8938c350d03cb8cd5a8a1947e41ec940be59e
Joltheim <james.wismer24@gmail.com>
2023-07-28 00:02:56 -0400

    Finished concValue & doMath func

--- Front End Libraries/P4-JS-Calc/js-calc/src/App.jsx
+++ Front End Libraries/P4-JS-Calc/js-calc/src/App.jsx
@@ -1,42 +1,58 @@
 import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState([])
+  const [result, setResult] = useState()
 
+  //Create String of userInput Values
   const concatValue = (e) => {
-    setCount(count + e.target.value)
-    console.log(count)
+    const lastChar = count[count.length - 1]
+      if (lastChar != '+' && lastChar != '-' && lastChar != '*' && lastChar != '/') {
+        setCount(count + e.target.value)
+      } else if (e.target.value != '+' && e.target.value != '-' && e.target.value != '*' && e.target.value != '/' ) {
+        setCount(count + e.target.value)
+      }
   }
 
   const clearScreen = () => {
-    setCount(0)
+    setCount([])
+    setResult()
+  }
+
+  const backSpace = () => {
+    const removeLastIndex = count.slice(0, count.length - 1)
+    setCount(removeLastIndex)
   }
 
   const doMath = () => {
-    let result = Math(count)
-    console.log(result)
+    if (count.length !== 0) {
+      setResult(eval(count))
+    }
   }
 
   return (
     <>
       <div className='calculator'>
         <div className='c-formula'>{count}</div>
-        <div className='c-screen'>answer: </div>
-        <button value='1' className='c-btn' onClick={concatValue}>1</button>
-        <button value='2' className='c-btn' onClick={concatValue}>2</button>
-        <button value='3' className='c-btn' onClick={concatValue}>3</button>
-        <button value='4' className='c-btn' onClick={concatValue}>4</button>
-        <button value='5' className='c-btn' onClick={concatValue}>5</button>
-        <button value='6' className='c-btn' onClick={concatValue}>6</button>
+        <div className='c-screen'>{result}</div>
         <button value='7' className='c-btn' onClick={concatValue}>7</button>
         <button value='8' className='c-btn' onClick={concatValue}>8</button>
         <button value='9' className='c-btn' onClick={concatValue}>9</button>
-        <button value='0' className='c-btn' onClick={concatValue}>0</button>
         <button value='+' className='c-btn' onClick={concatValue}>+</button>
+        <button value='4' className='c-btn' onClick={concatValue}>4</button>
+        <button value='5' className='c-btn' onClick={concatValue}>5</button>
+        <button value='6' className='c-btn' onClick={concatValue}>6</button>
         <button value='-' className='c-btn' onClick={concatValue}>-</button>
+        <button value='1' className='c-btn' onClick={concatValue}>1</button>
+        <button value='2' className='c-btn' onClick={concatValue}>2</button>
+        <button value='3' className='c-btn' onClick={concatValue}>3</button>
         <button value='*' className='c-btn' onClick={concatValue}>*</button>
-        <button value='/' className='c-btn' onClick={concatValue}>/</button>
+        <button value='0' className='c-btn' onClick={concatValue}>0</button>
         <button value='.' className='c-btn' onClick={concatValue}>.</button>
+        <button className='c-btn' onClick={backSpace}>undo</button>
+        <button value='/' className='c-btn' onClick={concatValue}>/</button>
+        <button value='(' className='c-btn' onClick={concatValue}>(</button>
+        <button value=')' className='c-btn' onClick={concatValue}>)</button>
         <button className='c-btn' onClick={clearScreen}>clear</button>
         <button className='c-btn' onClick={doMath}>enter</button>
       </div>
