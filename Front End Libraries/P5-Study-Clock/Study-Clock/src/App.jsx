f442de7e5f89929a36fa446212e9e73c85245d6d
Joltheim <james.wismer24@gmail.com>
2023-12-11 01:34:51 -0500

    added audio alarm and worked on countdown function

--- Front End Libraries/P5-Study-Clock/Study-Clock/src/App.jsx
+++ Front End Libraries/P5-Study-Clock/Study-Clock/src/App.jsx
@@ -1,14 +1,16 @@
 import { useState, useEffect } from 'react'
+import alarm1 from './assets/alarm1.mp3'
 
 function App() {
   const [studyTimer, setStudyTimer] = useState(25)
   const [breakTimer, setBreakTimer] = useState(5)
   const [countDownDisplay, setCountDownDisplay] = useState(studyTimer)
+  const [playCountDown, setPlayCountDown] = useState(false)
 
+  //formats studyTimer to HH:MM:SS format in countDownDisplay
   useEffect(() => {
     const convertCountDownDisplay = () => {
       const seconds = studyTimer * 60;
-  
       const formatTime = (value) => {
         return value < 10 ? `0${value}` : value.toString();
       };
@@ -28,7 +30,37 @@ function App() {
   
     convertCountDownDisplay();
   }, [studyTimer]);
+
+  //Countdown function toggled by playCountdown state
+  useEffect(() => {
+    if (!playCountDown) {
+      return;
+    }
   
+    let studyTimerSeconds = studyTimer * 60;
+  
+    const countdownInterval = setInterval(() => {
+      if (studyTimerSeconds > 0) {
+        studyTimerSeconds -= 1;
+        console.log(studyTimerSeconds);
+      } else {
+        clearInterval(countdownInterval);
+        const alarmAudio = new Audio(alarm1)
+        alarmAudio.play(alarm1)
+      }
+    }, 1000);
+  
+    return () => {
+      // Cleanup function to clear the interval when the component unmounts
+      clearInterval(countdownInterval);
+    };
+  }, [playCountDown, studyTimer]);
+  
+
+  const resetTimer = () => {
+    setStudyTimer(25)
+    setBreakTimer(5)
+  }
 
   return (
     <>
@@ -39,14 +71,14 @@ function App() {
             <h3>Study Timer: {studyTimer}</h3>
             <div className='flex-row'>
               <button id='up-arrow' onClick={() => setStudyTimer(studyTimer + 1)}>+1</button>
-              <button id='up-arrow' onClick={() => setStudyTimer(studyTimer - 1)}>-1</button>
+              <button id='up-arrow' onClick={() => studyTimer <= 0 ? null : setStudyTimer(studyTimer - 1)}>-1</button>
             </div>
           </div>
           <div id='set-timer'>
             <h3>Break Timer: {breakTimer}</h3>
             <div className='flex-row'>
               <button id='up-arrow' onClick={() => setBreakTimer(breakTimer + 1)}>+1</button>
-              <button id='up-arrow' onClick={() => setBreakTimer(breakTimer - 1)}>-1</button>
+              <button id='up-arrow' onClick={() => breakTimer <= 0 ? null : setBreakTimer(breakTimer - 1)}>-1</button>
             </div>
           </div>
         </div>
@@ -55,9 +87,9 @@ function App() {
         </div>
         <div className='control-panel'>
           <div className='flex-row'>
-            <button>play</button>
-            <button>pause</button>
-            <button>reset</button>
+            <button onClick={() => setPlayCountDown(true)}>play</button>
+            <button onClick={() => setPlayCountDown(false)}>pause</button>
+            <button onClick={() => resetTimer()}>reset</button>
           </div>
         </div>
         <div className='trade-mark'>
@@ -72,8 +104,8 @@ export default App
 
 //Todo:
 //1. Add buttons & functionality to increase / decrease timers - done
-  //1A. set limit so values cannot be negative
-//2. Add variable to show countdown status (setTimeOut?)
-//3. Create functions for control panel (play pause reset)
-//4. Add Alarm when countDownDisplay reaches 0
+  //1A. set limit so values cannot be negative - done (ternary operator)
+//2. Add variable to show countdown status - done (useEffect convertCountdownDisplay)
+//3. Create functions for control panel (play pause reset) - in progress
+//4. Add Audio Alarm when countDownDisplay reaches 0 - done
 //5. Make sure project passes FCC tests
