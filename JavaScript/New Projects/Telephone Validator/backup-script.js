b408aaa9675a83426f5f99e3cd25325bd49bb19d
Joltheim <james.wismer24@gmail.com>
2024-02-14 23:51:26 -0500

    updated telephone validator script.js w/o mapping

--- JavaScript/New Projects/Telephone Validator/backup-script.js
+++ JavaScript/New Projects/Telephone Validator/backup-script.js
@@ -3,7 +3,10 @@ const checkBtn = document.getElementById("check-btn")
 const clearBtn = document.getElementById("clear-btn")
 const resultsField = document.getElementById("results-div")
 
+let phoneNumberArr = []
+
 const clearNumbers = () => {
+        phoneNumberArr = []
         resultsField.innerHTML = ""
         userInput.value = ""
 }
@@ -15,6 +18,12 @@ const enterKey = (e) => {
 }
 
 const checkValidity = () => {
+    //add userInput to phone # array & keep limit to 5
+    phoneNumberArr.unshift(userInput.value)
+    if(phoneNumberArr.length > 5) {
+        phoneNumberArr.pop()
+    }
+
     //error popup for empty userInput field
     if(!userInput.value) {
         alert("Please provide a phone number")
@@ -23,10 +32,13 @@ const checkValidity = () => {
 
     //check for phone number format validity
     let regExp = /^1?[ -]?\(?\d{3}\)?[- ]?\d{3}[- ]?\d{4}$/gi
-    let isValid = regExp.test(userInput.value)
 
     //returned HTML    
-    resultsField.innerHTML += `<p>${isValid ? "Valid" : "Invalid"} US number: ${userInput.value}</p>`
+        resultsField.innerHTML = phoneNumberArr.map((str) => {
+            let isValid = regExp.test(str)
+            regExp.lastIndex = 0;
+            return `<p>${isValid ? "Valid" : "Invalid"} US number: ${str}</p>`
+    }).join('')
 
     userInput.value = ""
 }
@@ -35,9 +47,5 @@ checkBtn.addEventListener("click", checkValidity )
 document.addEventListener("keydown", enterKey)
 clearBtn.addEventListener("click", clearNumbers)
 
-//failed tests
-//1. 555)555-5555 - missing open par
-//2. (555-555-5555 - missing closing par
-
-//orig regExp = /^1?[ -]?\(?\d{3}\)?[- ]?\d{3}[- ]?\d{4}$/gi
-//let regExp = /^1?[ -]?    (\(? \d{3} \)?)    [- ]?\d{3}[- ]?\d{4}$/gi
+//ISSUE - Array Mapping method would not pass FCC tests
+//Solution - Deprecated file to backup-script.js and created new script.js file
