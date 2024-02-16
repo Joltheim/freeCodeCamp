96f8dcd242cccdc02880d4c387688e9c349ee2f8
Joltheim <james.wismer24@gmail.com>
2024-02-15 23:45:19 -0500

    completed telephone validator & added section explaining regex pattern

--- JavaScript/New Projects/Telephone Validator/script.js
+++ JavaScript/New Projects/Telephone Validator/script.js
@@ -21,23 +21,27 @@ const checkValidity = () => {
         return
     }
 
-    //check for phone number format validity
-    let regExp = /^1?[ -]?\(?\d{3}\)?[- ]?\d{3}[- ]?\d{4}$/gi
+    //check for phone number format validity - see notes below
+    let regExp = /^1?[- ]?(\((?=\d{3}\)))?(\d{3})((?<=\(\d{3})\))?[- ]?(\d{3})[- ]?(\d{4})$/
     let isValid = regExp.test(userInput.value)
 
     //returned HTML    
     resultsField.innerHTML += `<p>${isValid ? "Valid" : "Invalid"} US number: ${userInput.value}</p>`
-
     userInput.value = ""
 }
 
-checkBtn.addEventListener("click", checkValidity )
+checkBtn.addEventListener("click", checkValidity)
 document.addEventListener("keydown", enterKey)
 clearBtn.addEventListener("click", clearNumbers)
 
-//failed tests
-//1. 555)555-5555 - missing open par
-//2. (555-555-5555 - missing closing par
-
-//orig regExp = /^1?[ -]?\(?\d{3}\)?[- ]?\d{3}[- ]?\d{4}$/gi
-//let regExp = /^1?[ -]?    (\(? \d{3} \)?)    [- ]?\d{3}[- ]?\d{4}$/gi
+//Regex Pattern Breakdown - ln 25
+// ^ expression match starts line with following regex pattern
+// 1? optional (denoted by ?) match to international number 1
+// [- ]? optional inclusion of space or hyphen
+// (\((?=\d{3}\)))? optional capture group for escaped "(" char, w/ positive lookahead for 3 digits and closing ")"
+// (\d{3}) three digits in a row
+// ((?<=\(\d{3})\))? optional capture group for escaped ")", w/ positive lookbehind for 3 digits and open "("
+// [- ]? optional inclusion of space or hyphen
+// (\d{3}) three digits in a row
+// [- ]? optional inclusion of space or hyphen
+// (\d{4})$ four digits in a row with $ char to signify regex pattern match preceding it
